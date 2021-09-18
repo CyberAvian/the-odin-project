e30fe8749d2ff45c68de41290a57b70ea7aca894
cyberavian <cyberavian025@gmail.com>
2021-09-17 20:54:02 -0400

    Added simple UI

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,4 +1,3 @@
-// const MOVES = ['rock', 'paper', 'scissors'];
 const MOVES = {
     'rock': {
         'win': ['scissors'],
@@ -14,73 +13,100 @@ const MOVES = {
     }
 }
 
-let score = {
-    'tie': 0,
-    'win': 0,
-    'lose': 0
-};
-
-const main = () => {
-    rounds = prompt('How many rounds do you want to play (5 is the default)? ');
-    if (!rounds) {
-        rounds = 5;
-    }
-    game(rounds);
+let scores = {
+    'playerScore': 0,
+    'computerScore': 0,
+    'ties': 0,
+    'gamesWon': 0,
+    'gamesLost': 0
 }
 
-const game = (totalRounds) => {
-    for (let round = 0; round < totalRounds; round++) {
-        let computerMove = getComputerMove();
-        let playerMove = getPlayerMove();
-        console.log(playRound(playerMove, computerMove));
-    }
+const resultElement = document.createElement("p");
+const playerScoreElement = document.createElement("p");
+const computerScoreElement = document.createElement("p");
+const tieScoreElement = document.createElement("p");
+const gamesWonElement = document.createElement("p");
+const gamesLostElement = document.createElement("p");
 
-    console.log(`You tied ${score['tie']} out of ${totalRounds} games!`);
-    console.log(`You won ${score['win']} out of ${totalRounds} games!`);
-    console.log(`You lost ${score['lose']} out of ${totalRounds} games!`);
+function game() {
+    resetGame();
+
+    const roundScores = document.querySelector("#roundScores>ul");
+    const gamesScores = document.querySelector("#gamesScores>ul");
+
+    roundScores.appendChild(resultElement);
+    roundScores.appendChild(playerScoreElement);
+    roundScores.appendChild(computerScoreElement);
+    roundScores.appendChild(tieScoreElement);
+    gamesScores.appendChild(gamesWonElement);
+    gamesScores.appendChild(gamesLostElement);
+    
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.addEventListener('click', playRound));
 }
 
-const getComputerMove = () => {
+function resetGame() {
+    resultElement.textContent = "Round Result: ";
+
+    scores['playerScore'] = 0;
+    scores['computerScore'] = 0;
+    scores['ties'] = 0;
+
+    setRoundScores();
+    setGamesScores();
+}
+
+function setRoundScores() {
+    playerScoreElement.textContent = `Player Score: ${scores['playerScore']}`;
+    computerScoreElement.textContent = `Computer Score: ${scores['computerScore']}`;
+    tieScoreElement.textContent = `Ties: ${scores['ties']}`;
+}
+
+function setGamesScores() {
+    gamesWonElement.textContent = `Games Won: ${scores['gamesWon']}`;
+    gamesLostElement.textContent = `Games Lost: ${scores['gamesLost']}`;
+}
+
+function getComputerMove() {
     let computerMoveNum = Math.floor(Math.random() * 3);
     let computerMoveName = Object.keys(MOVES)[computerMoveNum];
 
     return computerMoveName;
 }
 
-const getPlayerMove = () => {
-    let playerInput;
-    while (true) {
-        playerInput = prompt("Select your move: ").toLowerCase();
-
-        if (playerInput in MOVES) {
-            break;
-        }
-        console.log(`${playerInput} is not valid.`)
-        console.log("Select from rock, paper, or scissors.");
-    }
-    return playerInput;
-}
-
-const playRound = (playerSelection, computerSelection) => {
-    console.log(`Computer chooses ${computerSelection}!`);
-    console.log(`Player chooses ${playerSelection}!`);
+function playRound(e) {
+    let playerSelection = e.target.id;
+    let computerSelection = getComputerMove();
 
     if (playerSelection == computerSelection) {
-        // Tie
-        score['tie'] += 1;
-        return `It's a tie. :/\n${playerSelection} ties with ${computerSelection}.`;
+        scores['ties'] += 1;
+        resultElement.textContent = `Round Result: It's a tie. :/\n${playerSelection} ties with ${computerSelection}.`;
     } else if (MOVES[playerSelection]['win'].includes(computerSelection)) {
-        // Win
-        score['win'] += 1;
-        return `You win! :)\nYour ${playerSelection} beats the computer's ${computerSelection}!`;
+        scores['playerScore'] += 1;
+        resultElement.textContent = `Round Result: You win! :)\nYour ${playerSelection} beats the computer's ${computerSelection}!`;
     } else if (MOVES[playerSelection]['lose'].includes(computerSelection)) {
-        // Lose
-        score['lose'] += 1;
-        return `You lose. :(\nThe computer's ${computerSelection} beat your ${playerSelection}.`
+        scores['computerScore'] += 1;
+        resultElement.textContent = `Round Result: You lose. :(\nThe computer's ${computerSelection} beat your ${playerSelection}.`
     } else {
-        // Something is wrong
-        return `Something went horribly wrong.`;
+        resultElement.textContent = `Round Result: Something went horribly wrong.`;
+    }
+
+    setRoundScores();
+    checkVictory();
+}
+
+function checkVictory() {
+    if (scores['playerScore'] === 5 || scores['computerScore'] === 5) {
+        if (scores['playerScore'] === 5) {
+            alert('You won!');
+            scores['gamesWon'] += 1;
+        } else {
+            alert('You lost!');
+            scores['gamesLost'] += 1;
+        }
+
+        resetGame();
     }
 }
 
-main();
+game();
